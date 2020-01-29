7ca030161d691c34b5489e3f8bef4ce8ad1e9525
antho1404 <anthony@mesg.com>
2020-01-29 16:37:26 +0700

    initial comit

---
+++ script.js
@@ -0,0 +1,35 @@
+async function fetchSpreadsheetData(url, cols) {
+  const resp = await fetch(url)
+  const data = await resp.json()
+  const res = []
+  for (const entry of data.feed.entry) {
+    const cell = entry["gs$cell"]
+    if (cell.row === "1") {
+      continue
+    }
+    res[cell.row - 2] = res[cell.row - 2] || {}
+    res[cell.row - 2][cols[cell.col - 1]] = cell["$t"] === 'TRUE'
+      ? true
+      : cell["$t"] === 'FALSE'
+        ? false
+        : cell["$t"]
+  }
+  return res
+    .filter(x => x)
+}
+
+async function load() {
+  const items = await fetchSpreadsheetData(
+    'https://spreadsheets.google.com/feeds/cells/1OS1cIxGmVGwqvwXs0bU0qzCDkZIN-Yrsw3Dx5scFy8c/1/public/full?alt=json',
+    ['name', 'url', 'category', 'payoutType', 'payMethod', 'fees', 'openSourceFocus', 'devFocus', 'short', 'description', 'logo']
+  )
+
+  new Vue({
+    el: '.list',
+    data: {
+      items
+    }
+  })
+}
+
+load()
