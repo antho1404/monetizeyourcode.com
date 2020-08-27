9705027e4b21783695d206327f6d1e535e433d8a
antho1404 <anthony.estebe@gmail.com>
2020-08-27 12:32:44 +0700

    use liteflow database

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
-async function fetchSpreadsheetData(url, cols) {
-  const resp = await fetch(url)
-  const data = await resp.json()
-  const res = []
-  for (const entry of data.feed.entry) {
-    const cell = entry["gs$cell"]
-    if (cell.row === "1") {
-      continue
-    }
-    res[cell.row - 2] = res[cell.row - 2] || {}
-    res[cell.row - 2][cols[cell.col - 1]] = cell["$t"] === 'TRUE'
-      ? true
-      : cell["$t"] === 'FALSE'
-        ? false
-        : cell["$t"]
-  }
-  return res
-    .filter(x => x)
-}
-
-async function load() {
-  const items = await fetchSpreadsheetData(
-    'https://spreadsheets.google.com/feeds/cells/1OS1cIxGmVGwqvwXs0bU0qzCDkZIN-Yrsw3Dx5scFy8c/1/public/full?alt=json',
-    ['id', 'name', 'url', 'category', 'payoutType', 'payMethod', 'fees', 'openSourceFocus', 'devFocus', 'screenshot', 'short', 'description', 'logo']
-  )
-
-  new Vue({
-    el: '.list',
-    data: {
-      items: items.map(x => ({ ...x, open: false }))
-    },
-    methods: {
-      toggle(item) {
-        item.open = !item.open
-        if (item.open) {
-          dataLayer.push({
-            id: item.id,
-            category: item.category,
-            type: 'open'
-          })
-        }
+new Vue({
+  el: ".list",
+  data: function () {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    toggle(item) {
+      item.open = !item.open;
+      if (item.open) {
+        dataLayer.push({
+          id: item.id,
+          category: item.category,
+          type: "open",
+        });
       }
-    }
-  })
-}
-
-load()
+    },
+  },
+  mounted() {
+    fetch("https://gateway.liteflow.com/af07defd-730f-4f8f-9593-34f6148d3c48", {
+      method: "POST",
+    })
+      .then((x) => x.json())
+      .then((x) => x.outputs.values.map((y) => y.value))
+      .then((x) => (this.items = x));
+  },
+});
